09b73139aa0e4aa1a1293e23199bbccbc1203f9d
Andrew112236 <102410886+Andrew112236@users.noreply.github.com>
2023-06-05 11:13:29 +0300

    js-modified

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,6 @@ const fontSize = document.getElementById("font-size-control");
 const textSpan = document.getElementById("text");
 
 fontSize.addEventListener("input", () => {
-  const fontSize = fontSize.value + "px";
-  textSpan.style.fontSize = fontSize;
+  const fontSize = fontSize.value;
+  textSpan.style.fontSize = fontSize + "px";
 });
